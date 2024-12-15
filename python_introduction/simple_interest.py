ccf255498f2029f63d2f97b8b001f0b1b972e699
Godwin731peter <chinegbomkpapeter@gmail.com>
2024-12-15 12:35:33 -0800

    Simple Interest Calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Variable names
-#principal(P)
-#rate(R)
-#time(T)
-#interest(I)
-P = 1000
-R = 0.05
-T = 3
-I = float(P * R * T)
-#printing the code
-print(f"The simple interest is: {I}")
+principal = 1000
+rate = 0.05
+time = 3
+#calculating simple interest
+interest = principal * rate * time
+#printing result
+print(f"The simple interest is: {interest}")
