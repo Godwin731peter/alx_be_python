6de89ce2cfd32737b7c330256519d7710fc1ed4a
Godwin731peter <chinegbomkpapeter@gmail.com>
2024-12-15 11:24:21 -0800

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Variable names
+number1 = 10
+number2 = 5
+# Introducing basic arithmetic operators
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+# Print result
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
