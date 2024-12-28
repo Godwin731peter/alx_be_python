a44ee7505ed6debc2a3b3b83e86adf18ee2654ff
petergodwin <chinegbomkpapeter@gmail.com>
2024-12-28 18:23:30 +0100

    Personal Daily Reminder

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# User indication variable
+task = input("Enter your task:")
+priority = input("Priority (high/medium/low):")
+time_bound = input("Is it time-bound?")
+
+match priority:
+  case "high":
+    reminder = f"{task} is a high priority task"
+  case "medium":
+    reminder = f"{task} is a medium priority task"
+  case "low":
+    reminder = f"{task} is a low priority task"
+  
+if time_bound == "yes":
+  print(f"Reminder: {reminder} that requires immediate attention today!")
+else:
+  print(f"Note: {reminder}. Consider completing it when you have free time")
