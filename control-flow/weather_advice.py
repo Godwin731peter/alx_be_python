fb7784a857ea63206e206cddedb15c8a4f54ad34
petergodwin <chinegbomkpapeter@gmail.com>
2024-12-28 13:22:46 +0100

    made changes

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Weather conditions 
-input("What's the weather like today? (sunny/rainy/cold):").strip().lower()
+weather = input("What's the weather like today? (sunny/rainy/cold):").strip().lower()
 
 # condition statement
-if "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
