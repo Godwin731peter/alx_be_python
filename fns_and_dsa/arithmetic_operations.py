0455e787bf6666289e559969934da8965e275188
petergodwin <chinegbomkpapeter@gmail.com>
2025-01-12 20:07:46 +0100

    Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+def perform_operation(num1, num2, operation):
+  if operation == "add":
+    return num1 + num2
+  elif operation == "subtract":
+    return num1 - num2
+  elif operation == "multiply":
+    return num1 * num2
+  elif operation == "divide":
+    if num2 == 0:
+      return "Error: division by zero not allowed"
+    else:
+      return num1/num2
+  else:
+    return "Error invalid operation"
