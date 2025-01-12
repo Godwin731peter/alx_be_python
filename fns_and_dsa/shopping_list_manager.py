40e22b626a8992f52af28d04906d26dc742c77e2
petergodwin <chinegbomkpapeter@gmail.com>
2025-01-12 21:06:06 +0100

    Shopping List Manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+shopping_list = []
+
+while True:
+  display_menu()
+  choice = input("Enter your choice: ").strip()
+
+  if choice == "1":
+    item = input("Enter the name of the item to add: ").strip()
+    if item:
+      shopping_list.append(item)
+      print(f"'{item}' has been added to the list")
+    else:
+      print("item name is empty")
+  elif choice == "2":
+    item = input("Enter the name of item to remove").strip()
+    if item in shopping_list:
+      shopping_list.remove(item)
+      print(f"'{item}' has been removed from shopping list")
+    else:
+      print("item is not in shopping list")
+  elif choice == "3":
+    if shopping_list:
+      print("current shoping list")
+      for index, item in enumerate(shopping_list, start=1):
+        print(f"{index}. {item}")
+    else:
+      print("The shopping list is currently empty")
+  elif choice == "4":
+    print("Goodbye!")
+  else:
+    print("Invalid choice. Please try again.")
