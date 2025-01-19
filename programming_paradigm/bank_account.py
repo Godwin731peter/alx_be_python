2c14f1930090b6a5a34f463a83e6b3a8026db5aa
petergodwin <chinegbomkpapeter@gmail.com>
2025-01-19 20:00:35 +0100

    corrected the self instance in withdraw

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,7 +8,7 @@ class BankAccount:
   
   def withdraw(self, amount):
     if self.account_balance > amount:
-      BankAccount.account_balance -= amount
+      self.account_balance -= amount
       return True
     else:
       return False
